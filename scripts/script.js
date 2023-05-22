0cc1b9b76767ced4a38d932dbf92b711e7b50819
Abdulloh <abduvohidov.abdullokh@gmail.com>
2023-05-22 17:29:10 +0500

    add section and finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,16 @@
 let submit = document.querySelector('#btnSubmit');
+// let form = document.querySelectorAll('.input-form');
+let fname = document.querySelector('#fname'),
+    lname = document.querySelector('#lname'),
+    c = document.querySelector('#mail'),
+    number = document.querySelector('#number');
 
 submit.addEventListener('click', () => {
-    alert('Request sent successfully');
+    if(!fname.value + !lname.value + !number.value + !mail.value == '') {
+        alert('Request sent successfully!');
+    }else{
+        alert('Please fill out the form!');
+    }
 })
+
+console.log(submit);
