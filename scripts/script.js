c03cf39cbf53aae574b5746473f9567d49e2c62f
Abdulloh <abduvohidov.abdullokh@gmail.com>
2023-05-31 00:53:49 +0500

    responsive

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,10 @@ let submit = document.querySelector('#btnSubmit');
 // let form = document.querySelectorAll('.input-form');
 let fname = document.querySelector('#fname'),
     lname = document.querySelector('#lname'),
-    c = document.querySelector('#mail'),
     number = document.querySelector('#number');
 
 submit.addEventListener('click', () => {
-    if(!fname.value + !lname.value + !number.value + !mail.value == '') {
+    if(!fname.value + !lname.value + !number.value == '') {
         alert('Request sent successfully!');
     }else{
         alert('Please fill out the form!');
